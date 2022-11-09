dd26c4d0621c3ba09821f88b92f8f2e295396cf3
Maksim Zuev <maksim.zuev@jetbrains.com>
2022-11-10 00:49:50 +0100

    Filter messages in single media group

--- notify.py
+++ notify.py
@@ -1,26 +1,31 @@
 import asyncio
+import logging
 from asyncio import sleep
 
 from config import notification_timeout_s, notification_single_timeout_s
 from data.news import *
 from data.subscriptions import *
 from data.users import *
+from scrolling import lock
 
 
 async def notify_user(bot, user_id):
-    user_subscriptions = get_subscriptions(user_id)
-    total = 0
-    for subscription in user_subscriptions:
-        posts = get_popular_posts(subscription.subscription, 3, subscription.last_seen_post)
-        if len(posts) == 0:
-            continue
-        posts.sort(key=lambda post: post.post_id)
-        last_post_id = posts[-1].post_id
-        update_last_seen_post(user_id, subscription.subscription, last_post_id)
-        for post in posts:
-            total += 1
-            await bot.send_message(user_id, f"https://t.me/{subscription.subscription}/{post.post_id}")
-            await sleep(notification_single_timeout_s)
+    async with lock:
+        user_subscriptions = get_subscriptions(user_id)
+        total = 0
+        for subscription in user_subscriptions:
+            posts = get_popular_posts(subscription.subscription, 3, subscription.last_seen_post)
+            if len(posts) == 0:
+                continue
+            posts.sort(key=lambda post: post.post_id)
+            last_post_id = posts[-1].post_id
+            update_last_seen_post(user_id, subscription.subscription, last_post_id)
+            for post in posts:
+                total += 1
+                link = f"https://t.me/{subscription.subscription}/{post.post_id}"
+                logging.info(f"Send message to {user_id}: {link} #comments={post.comments} #reactions={post.reactions}")
+                await bot.send_message(user_id, link)
+                await sleep(notification_single_timeout_s)
     return total
 
 
--- scrolling.py
+++ scrolling.py
@@ -11,43 +11,48 @@ from data.subscriptions import *
 
 app = Client("scroller", api_id=api_id, api_hash=api_hash)
 
+lock = asyncio.Lock()
 
 async def scheduled_scrolling():
     async with app:
         while True:
-            interesting_subscriptions = set()
-            for subscription in get_subscription_names():
-                interesting_subscriptions.add(subscription)
+            async with lock:
+                interesting_subscriptions = set()
+                for subscription in get_subscription_names():
+                    interesting_subscriptions.add(subscription)
 
-            # do not scroll messages earlier than one hour
-            soft_time_offset = datetime.datetime.now() - soft_time_window
-            hard_time_offset = datetime.datetime.now() - hard_time_window
-            for subscription in interesting_subscriptions:
-                has_comments = True
-                async for message in app.get_chat_history(chat_id=f"@{subscription}"):
-                    post_id = message.id
-                    timestamp = message.date
-                    if timestamp < hard_time_offset:
-                        break
-                    if timestamp < soft_time_offset and has_post(subscription, post_id):
-                        break
-                    reactions = 0
-                    if message.reactions is not None:
-                        reactions = sum([reaction.count for reaction in message.reactions.reactions])
-                    comments = 0
-                    if has_comments:
-                        try:
-                            comments = await app.get_discussion_replies_count(f"@{subscription}", post_id)
-                        except BadRequest:
-                            has_comments = False
-                            logging.warning(f"Failed to update comments in chanel {subscription} post {post_id}")
-                    add_post(Post(subscription, post_id, timestamp, comments, reactions))
-                    logging.info(f"Update chanel {subscription} post {post_id}, "
-                                 f"#comments={comments}, "
-                                 f"#reactions={reactions}")
-                    await sleep(scrolling_single_timeout_s)
+                # do not scroll messages earlier than one hour
+                soft_time_offset = datetime.datetime.now() - soft_time_window
+                hard_time_offset = datetime.datetime.now() - hard_time_window
+                for subscription in interesting_subscriptions:
+                    chat = await app.get_chat(chat_id=f"@{subscription}")
+                    has_comments = chat.linked_chat is not None
+                    async for message in app.get_chat_history(chat_id=f"@{subscription}"):
+                        post_id = message.id
+                        timestamp = message.date
+                        if timestamp < hard_time_offset:
+                            break
+                        if timestamp < soft_time_offset and has_post(subscription, post_id):
+                            break
+                        reactions = 0
+                        if message.reactions is not None:
+                            reactions = sum([reaction.count for reaction in message.reactions.reactions])
+                        comments = 0
+                        if has_comments:
+                            await sleep(scrolling_single_timeout_s)
+                            try:
+                                comments = await app.get_discussion_replies_count(f"@{subscription}", post_id)
+                            except BadRequest as e:
+                                if message.media_group_id is not None:
+                                    continue
+                                logging.warning(f"Failed to update comments in {message.link} {e.MESSAGE}")
+                        add_post(Post(subscription, post_id, timestamp, comments, reactions))
+                        logging.info(f"Update chanel {subscription} post {post_id}, "
+                                     f"#comments={comments}, "
+                                     f"#reactions={reactions}")
+                        await sleep(scrolling_single_timeout_s)
 
-            delete_posts_before(hard_time_offset)
+                delete_posts_before(hard_time_offset)
             await sleep(scrolling_timeout_s)
 
 
