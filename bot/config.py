97eba5c7377abacb636045aaedaf4a190b03eca1
Maksim Zuev <maksim.zuev@jetbrains.com>
2022-11-13 08:44:25 +0100

    Configure with env variables

--- bot/config.py
+++ bot/config.py
@@ -5,16 +5,23 @@ bot_token = os.environ["BOT_TOKEN"]
 api_id = os.environ["API_ID"]
 api_hash = os.environ["API_HASH"]
 
+
+def get_env_or(key, default_value):
+    if key in os.environ:
+        return os.environ[key]
+    return default_value
+
+
 MINUTE = 60
 initial_timeout_s = 5
-scrolling_timeout_s = 1 * MINUTE
+scrolling_timeout_s = int(get_env_or("SCROLLING_TIMEOUT_MIN", 10)) * MINUTE
 scrolling_single_timeout_s = 0.5
 
 hard_time_window = datetime.timedelta(days=1)
-soft_time_window = datetime.timedelta(hours=1)
+soft_time_window = datetime.timedelta(hours=3)
 news_drop_time = datetime.timedelta(weeks=1)
 
-notification_timeout_s = 5 * MINUTE
+notification_timeout_s = int(get_env_or("NOTIFICATION_TIMEOUT_MIN", 15)) * MINUTE
 notification_single_timeout_s = 0.5
 
 statistics_update_s = 10 * MINUTE
