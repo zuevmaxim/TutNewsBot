d5a7c1d0674099807c644e104b013965bf0084e4
Maksim Zuev <maksim.zuev@jetbrains.com>
2023-12-22 17:16:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
+if [ -f "env/local.env" ]; then
+  set -a
+  source env/local.env
+  set +a
+fi
+
+if [ -z "$POSTGRES_DB" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD" ] ||
+   [ -z "$POSTGRES_HOST" ] || [ -z "$POSTGRES_PORT" ]; then
+  echo "Missing required variables. Please check and re-run the script"
+  exit 1
+fi
+
 docker-compose -f docker-compose.yaml up -d
 
