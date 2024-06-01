6abe10b4ba74810810d6957fca1f193713e2cc1d
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-06-01 18:55:40 +0200

    Update standard config

--- bot/config.py
+++ bot/config.py
@@ -14,7 +14,7 @@ def get_env_or(key, default_value):
 
 MINUTE = 60
 initial_timeout_s = 5
-scrolling_timeout_s = int(get_env_or("SCROLLING_TIMEOUT_MIN", 10)) * MINUTE
+scrolling_timeout_s = int(get_env_or("SCROLLING_TIMEOUT_MIN", 11)) * MINUTE
 scrolling_single_timeout_s = 0.5
 notification_single_timeout_s = 1
 internal_error_timeout_s = 10
