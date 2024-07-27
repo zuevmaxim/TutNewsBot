37db3787d2cc52dd845872b4c59c7e23392d3ed1
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-07-27 10:02:42 +0200

    Reconnect to DB on lost connection

--- storage/postgres.py
+++ storage/postgres.py
@@ -14,7 +14,7 @@ class DB:
                  port: str,
                  database: str,
                  reconnect: bool = True,
-                 limit_reties: int = 0):
+                 limit_reties: int = 3):
         self.user = user
         self.password = password
         self.host = host
@@ -26,13 +26,13 @@ class DB:
 
         self._connection = None
 
-        self.init()
+        self.connect()
 
     def connect(self, retry_counter=0):
         if not self._connection:
             try:
                 self._connection = psycopg2.connect(user=self.user, password=self.password, host=self.host,
-                                                    port=self.port, database=self.database, connect_timeout=3, )
+                                                    port=self.port, database=self.database, connect_timeout=3)
                 retry_counter = 0
                 self._connection.autocommit = True
                 return self._connection
@@ -41,7 +41,7 @@ class DB:
                     raise error
                 else:
                     retry_counter += 1
-                    logging.error("Got error {}. Reconnecting {}.".format(str(error).strip(), retry_counter))
+                    logging.error(f"Got error {str(error).strip()}. Reconnecting {retry_counter}.")
                     time.sleep(5)
                     self.connect(retry_counter)
             except (Exception, psycopg2.Error) as error:
@@ -62,7 +62,7 @@ class DB:
                 raise error
             else:
                 retry_counter += 1
-                logging.error("Got error {}. Retrying {}.".format(str(error).strip(), retry_counter))
+                logging.error(f"Got error {str(error).strip()}. Retrying {retry_counter}.")
                 time.sleep(1)
                 self.reset()
                 return self.execute(query, vars, retry_counter)
@@ -78,9 +78,6 @@ class DB:
             self._connection.close()
             logging.info("PostgreSQL connection is closed.")
         self._connection = None
-
-    def init(self):
-        self.connect()
 
 
 db = DB(os.environ["POSTGRES_USER"],
