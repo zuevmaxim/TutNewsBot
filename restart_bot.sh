0197dafa06ae6f99532a8830631ad582507ee3fc
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-09-22 10:33:35 +0200

    Add script to restart bot

---
+++ restart_bot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -f "env/sync.env" ]; then
+  source env/sync.env
+fi
+if [ -z "$KEY_PATH" ] || [ -z "$TARGET_USER" ] || [ -z "$TARGET_HOST" ] || [ -z "$TARGET_PATH" ]; then
+  echo "Missing required variables. Please check and re-run the script"
+  exit 1
+fi
+
+echo "Stopping bot"
+ssh -i "$KEY_PATH" "$TARGET_USER@$TARGET_HOST" "docker stop tutnews-bot" || exit 2
+echo "Bot stopped"
+
+echo "Sync files with release bot"
+./sync.sh || exit 3
+
+echo "Restart bot"
+ssh -i "$KEY_PATH" "$TARGET_USER@$TARGET_HOST" "cd $TARGET_PATH && ./build.sh" || exit 4
+
+echo "Done"
+
