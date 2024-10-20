56a998dc049e2a9d390399138fd2540b128325c2
Maksim Zuev <maksim.zuev@jetbrains.com>
2024-10-20 21:41:40 +0200

    Add more percentile levels

--- bot/config.py
+++ bot/config.py
@@ -23,6 +23,6 @@ hard_time_window = datetime.timedelta(days=1)
 soft_time_window = datetime.timedelta(hours=6)
 news_drop_time = datetime.timedelta(weeks=1)
 
-INTERESTING_PERCENTILES = [75, 80, 90, 95, 99]
-DEFAULT_PERCENTILE = 90
+INTERESTING_PERCENTILES = [0, 25, 50, 75, 90, 95, 99]
+DEFAULT_PERCENTILE = 95
 logging_user = get_env_or("LOG_USER_ID", None)
